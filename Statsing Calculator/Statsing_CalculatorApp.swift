2327cb30dad6f776183641651705cd5562ffd838
LuckDev <lukedru@yahoo.com>
2022-10-12 11:18:15 -0700

    Initial Commit

---
+++ Statsing Calculator/Statsing_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Statsing_CalculatorApp.swift
+//  Statsing Calculator
+//
+//  Created by Luke Drushell on 10/12/22.
+//
+
+import SwiftUI
+
+@main
+struct Statsing_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
